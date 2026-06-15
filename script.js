a762d6aa3f33e9f5103748fda0788555442f238f
felicarol <234343054+felicarol@users.noreply.github.com>
2026-06-15 04:34:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("contactBtn").addEventListener("click", function(){
+
+    alert(
+        "Terima kasih telah mengunjungi CV saya.\n\nSilakan hubungi saya melalui email atau WhatsApp yang tertera."
+    );
+
+});
